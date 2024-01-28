81084295c38d1af7954c11735a61bc9c4361aa66
Niji Saeba Media Noche <mathereall@gmail.com>
2024-01-27 19:41:17 -0600

    added README.es.md

--- src/gdmty_drf_firebase_auth/__init__.py
+++ src/gdmty_drf_firebase_auth/__init__.py
@@ -1,15 +1,28 @@
-""" Custom Django Rest Framework authentication backend for parsing Firebase uid tokens or email/password, and storing as local users.'
+#!-*- coding: utf-8 -*-
+"""
+Authentication backend for handling firebase user.idToken from incoming
+Authorization header, verifying, and locally authenticating
 
-This package is a fork of django-firebase-auth
+This package are published as free software under the terms of the Apache License, Version 2.0. Is developed by
+Dirección de Gobierno Digital of the Secretaría de Innovación y Gobierno Abierto of Municipality of Monterrey.
+
+Authors: ['César Benjamín García Martínez <mathereall@gmail.com>', ]
+Email: gobiernodigital@monterrey.gob.mx
+GitHub: https://github.com/gobiernodigitalmonterrey/gdmty-drf-firebase-auth
+Package: gdmty_drf_firebase_auth
+PyPi: https://pypi.org/project/gdmty-drf-firebase-auth/
+License: Apache 2.0
+
+This package is a fork of drf-firebase-auth
 with some fixes and improvements.
 This package also adds support for login
 with email and password, and also adds support for
 custom user models. And is fully integrated with gdmty-id.
 
-
 """
+
 __title__ = 'gdmty_drf_firebase_auth'
-__version__ = '2023.12-dev5'
+__version__ = '24.1.0'
 __description__ = (
     'Custom Django Rest Framework authentication backend for '
     'parsing Firebase uid tokens and storing as local users.'
@@ -19,8 +32,8 @@ __description__ = (
     'with email and password, and also adds support for '
     'custom user models. And is fully integrated with idmty.'
 )
-__url__ = 'https://github.com/SIGAMty/gdmty-drf-firebase-auth'
-__author__ = 'César Benjamín'
+__url__ = 'https://github.com/gobiernodigitalmonterrey/gdmty-drf-firebase-auth'
+__author__ = 'César Benjamín García Martínez'
 __author_email__ = 'mathereall@gmail.com'
 __license__ = 'Apache 2.0'
 VERSION = __version__
--- src/gdmty_drf_firebase_auth/settings.py
+++ src/gdmty_drf_firebase_auth/settings.py
@@ -1,5 +1,20 @@
-# -*- coding: utf-8 -*-
-""" Configuración de settings para la aplicación mty_django_firebase_auth """
+#!-*- coding: utf-8 -*-
+"""
+Authentication backend for handling firebase user.idToken from incoming
+Authorization header, verifying, and locally authenticating
+
+This package are published as free software under the terms of the Apache License, Version 2.0. Is developed by
+Dirección de Gobierno Digital of the Secretaría de Innovación y Gobierno Abierto of Municipality of Monterrey.
+
+Authors: ['César Benjamín García Martínez <mathereall@gmail.com>', ]
+Email: gobiernodigital@monterrey.gob.mx
+GitHub: https://github.com/gobiernodigitalmonterrey/gdmty-drf-firebase-auth
+Package: gdmty_drf_firebase_auth
+PyPi: https://pypi.org/project/gdmty-drf-firebase-auth/
+License: Apache 2.0
+"""
+
+
 import os
 import ast
 from django.conf import settings
